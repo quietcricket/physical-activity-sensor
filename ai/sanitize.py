bca365417bcc6c6f5f71b9f0fc4457ca788744ea
Shang Liang <shang@wewearglasses.com>
2018-03-21 12:38:05 +0800

    Changed data smoothing function to flat weight from hanning

--- ai/sanitize.py
+++ ai/sanitize.py
@@ -1,11 +1,11 @@
-import numpy as np
 import os
+
 import matplotlib.pyplot as plt
-from datetime import datetime, timedelta
+import numpy as np
 
 
 def list_data(folder='data'):
-    return [f for f in os.listdir(folder) if f.endswith('txt')]
+    return [f for f in os.listdir(folder) if f.find('.log.') > -1]
 
 
 def reject_outliers(input_arr, m=2):
@@ -17,30 +17,28 @@ def clean_data(filename):
     arr = []
     digits = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
     for l in data_lines:
-        arr += [int(x) for x in l.split(',') if len(x) > 0 and x[0] in digits]
+        arr += [float(x) for x in l.split(',') if len(x) > 0 and x[0] in digits]
     arr = reject_outliers(np.asarray(arr))
-    weight = np.hanning(5)
+    window = 11
+    weight = np.ones(window) / float(window)
     out = np.convolve(arr, weight / weight.sum(), 'same')
-    f = open('clean-data/{}'.format(filename), 'w')
-    f.write(','.join([str(x) for x in out]))
-    f.close()
-
-    # x = np.arange(len(arr))
-    # plt.plot(x, out)
-    # plt.savefig('plot/{}.png'.format(filename))
-    # plt.clf()
+    output_file = open('clean-data/{}'.format(filename), 'w')
+    output_file.write(','.join([str(x) for x in out]))
+    output_file.close()
 
 
 def plot_data(filename):
     arr = [float(x) for x in open('clean-data/{}'.format(filename)).readline().split(',')]
     x = np.arange(len(arr))
     plt.plot(x, arr)
-    plt.savefig('plot/{}.png'.format(filename[:-4]))
+    fig = plt.gcf()
+    fig.set_size_inches(30, 10)
+    fig.savefig('plot/{}.png'.format(filename))
     plt.clf()
 
 
-# for f in list_data():
-#     clean_data(f)
+for f in list_data('data'):
+    clean_data(f)
 
 for f in list_data('clean-data'):
     plot_data(f)
